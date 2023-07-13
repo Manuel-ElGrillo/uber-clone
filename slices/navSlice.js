fd9d6301693f86c383eb87dbf79d41687da89a63
Manuel De Leon <manuel@deleonnet.com>
2023-07-13 18:53:24 -0300

    Redux setup

---
+++ slices/navSlice.js
@@ -0,0 +1,35 @@
+//Slice for navigation from point A (origin) to point B (destination)
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInfromation: null,
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: ( state, action ) => {
+            state.origin = action.payload
+        },
+        setDestination: ( state, action ) => {
+            state.destination = action.payload
+        },
+        setTravelTimeInformation: ( state, action ) => {
+            state.travelTimeInfromation = action.payload
+        }
+    }
+})
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions
+
+//Selectors
+
+export const selectOrigin = ( state ) => state.nav.origin
+export const selectDestination = ( state ) => state.nav.destination
+export const selectTravelTimeInformation = ( state ) => state.nav.travelTimeInfromation
+
+export default navSlice.reducer
